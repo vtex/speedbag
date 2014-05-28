d5a130812c0d8d4b4adae0054d9e7e74924652c7
Guilherme Rodrigues <gadr90@gmail.com>
2014-05-28 11:25:33 -0300

    Clean gh-pages

--- js/hello-controller.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  angular.module('app').controller('HelloController', [
-    '$scope', function($scope) {
-      return $scope.awesomeThings = ['Hello', 'World', '!'];
-    }
-  ]);
-
-}).call(this);
